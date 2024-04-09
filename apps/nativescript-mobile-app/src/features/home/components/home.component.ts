7a20cc5c866bd1a2863b8e00ae03c59356f4f5f1
Joshua Calafell <joshcalafell@gmail.com>
2024-04-09 03:09:05 -0700

    Sampling UI Components

--- apps/nativescript-mobile-app/src/features/home/components/home.component.ts
+++ apps/nativescript-mobile-app/src/features/home/components/home.component.ts
@@ -1,22 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, OnInit } from '@angular/core'
 import { setStatusBarColor } from '../../../utils'
-import { User, Game, Item } from '@angular-nativescript-workspace/shared'
+import { User } from '@angular-nativescript-workspace/shared'
 @Component({
   moduleId: module.id,
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  user: User = new User(1, 'Frankie Teardrop', 'frankie@gmail.com', 'consumer')
-  game: any = new Game(1, 'Monopoly')
-  item: any = new Item(1, 'Glass of Water')
+  user
+
+  loading = false
 
   ngOnInit() {
     setStatusBarColor('dark', '#97d9e9')
   }
 
   onTap() {
-    console.log('onTap')
+    this.loading = !this.loading
+    this.user = new User(
+      1,
+      'Josh Calafell',
+      'joshcalafell@gmail.com',
+      'consumer',
+    )
   }
 }
